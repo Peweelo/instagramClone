000ae243d6b847dd536070f7b5db57e84abfd06e
peweelo <peweelop@gmail.com>
2023-10-03 21:41:50 +0200

    chaning things for vercel 2

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,14 +1,12 @@
-import NextAuth from 'next-auth/next'
+import NextAuth, { AuthOptions } from 'next-auth'
 import prisma from '../../../libs/prismadb'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import CredentialsProvider from 'next-auth/providers/credentials'
-
 import bcrypt from 'bcrypt'
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
 	adapter: PrismaAdapter(prisma),
 	providers: [
-
 		CredentialsProvider({
 			name: 'credentials',
 			credentials: {
@@ -17,32 +15,29 @@ export const authOptions = {
 				password: { label: 'Password', type: 'password' },
 			},
 			async authorize(credentials) {
-				// check to see if email and password is there
-				if (!credentials.email || !credentials.password) {
-					throw new Error('Please enter an email and password')
+				if (credentials) {
+					if (!credentials.email || !credentials.password) {
+						throw new Error('Please enter an email and password')
+					}
+
+					const user = await prisma.user.findUnique({
+						where: {
+							email: credentials.email,
+						},
+					})
+
+					if (!user || !user?.hashedPassword) {
+						throw new Error('No user found')
+					}
+
+					const passwordMatch = await bcrypt.compare(credentials.password, user.hashedPassword)
+
+					if (!passwordMatch) {
+						throw new Error('Incorrect password')
+					}
+
+					return user
 				}
-
-				// check to see if user exists
-				const user = await prisma.user.findUnique({
-					where: {
-						email: credentials.email,
-					},
-				})
-
-				// if no user was found
-				if (!user || !user?.hashedPassword) {
-					throw new Error('No user found')
-				}
-
-				// check to see if password matches
-				const passwordMatch = await bcrypt.compare(credentials.password, user.hashedPassword)
-
-				// if password does not match
-				if (!passwordMatch) {
-					throw new Error('Incorrect password')
-				}
-
-				return user
 			},
 		}),
 	],
@@ -54,4 +49,5 @@ export const authOptions = {
 }
 
 const handler = NextAuth(authOptions)
+
 export { handler as GET, handler as POST }
