f693acebf71f0f6fbe7afd19a13f63a644e4c931
peweelo <peweelop@gmail.com>
2023-10-04 16:07:22 +0200

    fixed authoptions not working with vercel correctly

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,53 +1,6 @@
-import NextAuth, { NextAuthOptions, getServerSession } from 'next-auth'
-import prisma from '../../../libs/prismadb'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import bcrypt from 'bcrypt'
+import NextAuth from 'next-auth'
 
-const authOptions: NextAuthOptions = {
-	adapter: PrismaAdapter(prisma),
-	providers: [
-		CredentialsProvider({
-			name: 'credentials',
-			credentials: {
-				email: { label: 'Email', type: 'text', placeholder: 'jsmith@.com' },
-				login: { label: 'Login', type: 'text', placeholder: 'jsmith' },
-				password: { label: 'Password', type: 'password' },
-			},
-			async authorize(credentials) {
-				if (credentials) {
-					if (!credentials.email || !credentials.password) {
-						throw new Error('Please enter an email and password')
-					}
-
-					const user = await prisma.user.findUnique({
-						where: {
-							email: credentials.email,
-						},
-					})
-
-					if (!user || !user?.hashedPassword) {
-						throw new Error('No user found')
-					}
-
-					const passwordMatch = await bcrypt.compare(credentials.password, user.hashedPassword)
-
-					if (!passwordMatch) {
-						throw new Error('Incorrect password')
-					}
-
-					return user
-				}
-			},
-		}),
-	],
-	secret: process.env.SECRET,
-	session: {
-		strategy: 'jwt',
-	},
-	debug: process.env.NODE_ENV === 'development',
-}
-export const getAuthSession = () => getServerSession(authOptions)
+import { authOptions } from '@/app/authOptions'
 
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
---
+++ app/authOptions.tsx
@@ -0,0 +1,51 @@
+import NextAuth, { NextAuthOptions, getServerSession } from 'next-auth'
+import prisma from './libs/prismadb'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import bcrypt from 'bcrypt'
+
+export const authOptions: NextAuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		CredentialsProvider({
+			name: 'credentials',
+			credentials: {
+				email: { label: 'Email', type: 'text', placeholder: 'jsmith@.com' },
+				login: { label: 'Login', type: 'text', placeholder: 'jsmith' },
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials) {
+				if (credentials) {
+					if (!credentials.email || !credentials.password) {
+						throw new Error('Please enter an email and password')
+					}
+
+					const user = await prisma.user.findUnique({
+						where: {
+							email: credentials.email,
+						},
+					})
+
+					if (!user || !user?.hashedPassword) {
+						throw new Error('No user found')
+					}
+
+					const passwordMatch = await bcrypt.compare(credentials.password, user.hashedPassword)
+
+					if (!passwordMatch) {
+						throw new Error('Incorrect password')
+					}
+
+					return user
+				}
+			},
+		}),
+	],
+	secret: process.env.SECRET,
+	session: {
+		strategy: 'jwt',
+	},
+	debug: process.env.NODE_ENV === 'development',
+}
+
+export const getAuthSession = () => getServerSession(authOptions)
