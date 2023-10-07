fe0fd50706311e352760223b1621e21f0580e591
peweelo <peweelop@gmail.com>
2023-10-07 12:24:32 +0200

    fixing things for vercel

--- app/api/changeUserImage/route.tsx
+++ app/api/changeUserImage/route.tsx
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import prisma from '../../libs/prismadb'
-export async function POST(req: Response) {
+export async function POST(req: Request) {
 	const { userId, imageUrl } = await req.json()
 
 	console.log(userId, imageUrl)
